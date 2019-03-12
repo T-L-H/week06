0b202432e153406da91bf970ac9fd0638216ef8a
User <User@DEV-022774.OnBase.net>
2019-03-12 19:34:44 -0400

    first commit

---
+++ week 6 challenges/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace week_6_challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a number.");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            int x = 0;
+            while(x < num){
+                x++;
+           
+                if(x % 2 == 0){
+                    Console.WriteLine(x);
+                }
+            }
+        }
+    }
+}
